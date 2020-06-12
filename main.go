fee3c18bd4b168aeb3821c32e41d31c052f170db
tomoyaueno29 <u.t012807@gmail.com>
2020-06-12 19:15:32 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/lib/pq"
 )
 
 type Todo struct {
 	gorm.Model
-
+	Text   string
+	Status string
 }
 
-func main(){
+func dbInit() {
+	db, err := sql.Open("postgres", "username=tomoyaueno dbname=gogin password=gogin sslmode=disable")
+}
+
+func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
 	data := "Hello Go/Gin"
-	router.GET("/", func(ctx *gin.Context){
+	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(200, "index.html", gin.H{"data": data})
 	})
 
