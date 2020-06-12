1c705bbba5897a262eca06b5a9703027ac3d7bf8
tomoyaueno29 <u.t012807@gmail.com>
2020-06-12 19:33:16 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
@@ -21,6 +20,51 @@ func dbInit() {
 	}
 	Db.AutoMigrate(&Todo{})
 	defer Db.Close()
+}
+
+func dbInsert(text string, status string) {
+	Db, err := gorm.Open("postgres", "username=tomoyaueno dbname=gogin password=gogin sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	Db.Create(&Todo{Text: text, Status: status})
+	defer Db.Close()
+}
+
+func dbUpdate(id int, text string, status string) {
+	Db, err := gorm.Open("postgres", "username=tomoyaueno dbname=gogin password=gogin sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	var todo Todo
+	Db.First(&todo, id)
+	todo.Text = text
+	todo.Status = status
+	Db.Save(&todo)
+	Db.Close()
+}
+
+func dbDelete(id int) {
+	Db, err := gorm.Open("postgres", "username=tomoyaueno dbname=gogin password=gogin sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	var todo Todo
+	//引数のidを参照して最初のレコードを取得
+	Db.First(&todo, id)
+	Db.Delete(&todo)
+	Db.Close()
+}
+
+func dbGetAll() []Todo {
+	Db, err := gorm.Open("postgres", "username=tomoyaueno dbname=gogin password=gogin sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	var todos []Todo
+	Db.Order("created_at desc").Find(&todos)
+	Db.Close()
+	return todos
 }
 
 func main() {
